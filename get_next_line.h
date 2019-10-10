53e7c99942fbcc7c9f6fe68ad56db4c3a4d3bbb1
Gaspard Moindrot <gaspardmoindrot@gmail.com>
2019-10-10 16:30:55 +0200

    fin

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmoindro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/02 11:44:31 by gmoindro          #+#    #+#             */
+/*   Updated: 2019/06/17 15:41:27 by gmoindro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 100
+
+# include "libft/libft.h"
+
+int		get_next_line(int const fd, char **line);
+
+#endif
